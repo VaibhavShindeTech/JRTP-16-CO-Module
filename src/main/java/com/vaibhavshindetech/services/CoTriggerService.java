c26635d33a872fa75a72f35f2eb3f41a2cde3de3
Vaibhav Shinde <58801803+VaibhavShindeTech@users.noreply.github.com>
2022-04-06 23:00:08 +0530

    MTP6-3 space added back

--- src/main/java/com/vaibhavshindetech/services/CoTriggerService.java
+++ src/main/java/com/vaibhavshindetech/services/CoTriggerService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.vaibhavshindetech.entities.CoTriggerEntity;
 
 public interface CoTriggerService {
+	
 	public List<CoTriggerEntity> getAllPendingRecords();
 
 	public void updateCoTriggerStatus(CoTriggerEntity coTriggerEntity);
